ecec0c20c6e51bc11c91eba796144b60f8667e22
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-27 20:58:14 -0300

    feat: implement main activity code

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/MainActivity.kt
@@ -1,20 +1,23 @@
 package br.edu.ifsp.scl.ads.prdm.sc3038998.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3038998.imfitplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(amb.root)
+
+        amb.mainStartBt.setOnClickListener {
+            val intent = Intent(this, ProfileFormActivity::class.java)
+            startActivity(intent)
         }
     }
 }
