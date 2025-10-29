09f071cbba1ca38b034d2daa3bf41c0958c46638
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-29 19:45:57 -0300

    feat: implement imc result intent to next screen

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/ImcResultActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/ImcResultActivity.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.ads.prdm.sc3038998.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -27,12 +28,29 @@ class ImcResultActivity : AppCompatActivity() {
 
     private fun advance() {
         val name = intent.getStringExtra("EXTRA_NAME")
+        val age = intent.getIntExtra("EXTRA_AGE", 0)
+        val height = intent.getDoubleExtra("EXTRA_HEIGHT", 0.0)
+        val weight = intent.getDoubleExtra("EXTRA_WEIGHT", 0.0)
+        val sex = intent.getStringExtra("EXTRA_SEX")
+        val activityLevel = intent.getStringExtra("EXTRA_ACTIVITY_LEVEL")
         val imc = intent.getDoubleExtra("EXTRA_IMC", 0.0)
+
         val category = calculateCategory(imc)
 
         nameTv.text = name
         resultTv.text = imc.toString()
         categoryTv.text = category
+
+        val intentCR = Intent(this, CaloricResultActivity::class.java)
+        intentCR.putExtra("EXTRA_NAME", name)
+        intentCR.putExtra("EXTRA_AGE", age)
+        intentCR.putExtra("EXTRA_HEIGHT", height)
+        intentCR.putExtra("EXTRA_WEIGHT", weight)
+        intentCR.putExtra("EXTRA_SEX", sex)
+        intentCR.putExtra("EXTRA_ACTIVITY_LEVEL", activityLevel)
+        intentCR.putExtra("EXTRA_IMC", imc)
+
+        startActivity(intentCR)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
