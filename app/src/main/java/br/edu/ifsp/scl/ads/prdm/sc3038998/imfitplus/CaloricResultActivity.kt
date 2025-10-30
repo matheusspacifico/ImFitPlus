d0dcb55d49ed1e24aa3141fb3c825e83780b01ab
matheusspacifico <matheuspacifico44@gmail.com>
2025-10-30 00:32:06 -0300

    feat: calculate tmb and pass to next screen through intent

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/CaloricResultActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/CaloricResultActivity.kt
@@ -1,5 +1,6 @@
 package br.edu.ifsp.scl.ads.prdm.sc3038998.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -25,7 +26,32 @@ class CaloricResultActivity : AppCompatActivity() {
         return 655 + (9.6 * weight) + (1.8 * height * 100) - (4.7 * age)
     }
     private fun advance() {
+        val name = intent.getStringExtra("EXTRA_NAME")
+        val age = intent.getIntExtra("EXTRA_AGE", 0)
+        val height = intent.getDoubleExtra("EXTRA_HEIGHT", 0.0)
+        val weight = intent.getDoubleExtra("EXTRA_WEIGHT", 0.0)
+        val sex = intent.getStringExtra("EXTRA_SEX")
+        val activityLevel = intent.getStringExtra("EXTRA_ACTIVITY_LEVEL")
+        val imc = intent.getDoubleExtra("EXTRA_IMC", 0.0)
+        val category = intent.getStringExtra("EXTRA_CATEGORY")
 
+        val tmb = calculateTmb(sex ?: "Masculino", weight, height, age)
+
+        nameTv.text = name
+        resultTv.text = tmb.toString()
+
+        val intentIW = Intent(this, IdealWeightActivity::class.java)
+        intentIW.putExtra("EXTRA_NAME", name)
+        intentIW.putExtra("EXTRA_AGE", age)
+        intentIW.putExtra("EXTRA_HEIGHT", height)
+        intentIW.putExtra("EXTRA_WEIGHT", weight)
+        intentIW.putExtra("EXTRA_SEX", sex)
+        intentIW.putExtra("EXTRA_ACTIVITY_LEVEL", activityLevel)
+        intentIW.putExtra("EXTRA_IMC", imc)
+        intentIW.putExtra("EXTRA_CATEGORY", category)
+        intentIW.putExtra("EXTRA_TMB", tmb)
+
+        startActivity(intentIW)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
