730ab8568b1f6d9a427eb9e3ce3adbb74851d093
matheusspacifico <matheuspacifico44@gmail.com>
2025-12-10 20:45:38 -0300

    fix: fix user calculation records table name

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/entity/UserCalculationRecord.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/entity/UserCalculationRecord.kt
@@ -3,7 +3,7 @@ package br.edu.ifsp.scl.ads.prdm.sc3038998.imfitplus.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "calculation_records")
+@Entity(tableName = "user_calculation_records")
 data class UserCalculationRecord(
 
     @PrimaryKey(autoGenerate = true)
