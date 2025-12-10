94b4f29066cb25cc9077783285a042ffb2350075
matheusspacifico <matheuspacifico44@gmail.com>
2025-12-10 20:31:34 -0300

    feat: add user entity

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3038998/imfitplus/entity/User.kt
@@ -0,0 +1,16 @@
+package br.edu.ifsp.scl.ads.prdm.sc3038998.imfitplus.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users")
+data class User(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String,
+    val age: Int,
+    val height: Double,
+    val weight: Double,
+    val activityLevel: String
+)
